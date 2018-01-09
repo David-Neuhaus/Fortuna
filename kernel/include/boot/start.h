e672d641cdcbb3637fcdfe56c25d452f64dd09bc
David Neuhaus <DavidNeuhaus@web.de>
2018-01-09 19:31:39 +0100

    Add some Doc files and startup headers

---
+++ kernel/include/boot/start.h
@@ -0,0 +1,9 @@
+#ifndef BOOT_START_H
+#define BOOT_START_h
+
+/* Default Stack and Heap Size, they grow towards each other
+ * so they are really twice as big, respectively*/
+#define HEAP_SIZE 4096
+#define STACK_SIZE 4096
+
+#endif
