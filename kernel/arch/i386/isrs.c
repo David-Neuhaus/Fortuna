f9e7f316b9c26e42a26df8398d185c8fe6a32ff3
David Neuhaus <DavidNeuhaus@web.de>
2019-01-13 22:15:58 +0100

    Interrupt handler higher half kernel support

--- kernel/arch/i386/isrs.c
+++ kernel/arch/i386/isrs.c
@@ -9,9 +9,9 @@ struct cpu_state* handle_exception(struct cpu_state* cpu){
 	printf(" in handle exception \n");
 	if(cpu->intr < 0x20){
 		printf("%s, Kernel stopped!\n", err_msg[cpu->intr]);
-		printf("CPU status: \n eax: \t%d\n ebx: \t%d\n ecx: \t%d\n edx: \t%d\n esi: \t%d\n \
-                edi: \t%d\n ebp: \t%d\n\nInterruptnummer: \t%d\nError-Code:\t%d\n\nAutomatisch \
-                gesichert: \n eip: \t%d\n cs: \t%d\n eflags: \t%d\n esp: \t%d\n ss: \t%d\n", cpu->eax, 
+		printf("CPU status: \n eax: \t%x\n ebx: \t%x\n ecx: \t%x\n edx: \t%x\n esi: \t%x\n "
+                "edi: \t%x\n ebp: \t%x\n\nInterruptnummer: \t%x\nError-Code:\t%u\n\nAutomatisch"
+                " gesichert: \n eip: \t%x\n cs: \t%x\n eflags: \t%x\n esp: \t%x\n ss: \t%x\n", cpu->eax, 
                 cpu->ebx, cpu->ecx, cpu->edx, cpu->esi, cpu->edi, cpu->ebp, cpu->intr, cpu->error, cpu->eip, cpu->cs, cpu->eflags, cpu->esp, cpu->ss);
 		while(1)
 			asm volatile("cli; hlt");
@@ -25,17 +25,17 @@ static void (*irq_handlers[16])(void*);
 struct cpu_state* handle_irq(struct cpu_state* cpu){
 	printf(" in handle_irq \n");
 	struct cpu_state* new_cpu = cpu;
-	if(cpu->intr >= 0x20 && cpu->intr <= 0x2f){
+	//if(cpu->intr >= 0x20 && cpu->intr <= 0x2f){
 
-        if(cpu->intr == 0x20)
-            irq_handlers[cpu->intr - 0x20]((void*) cpu);
+        //if(cpu->intr == 0x20)
+            //irq_handlers[cpu->intr - 0x20]((void*) cpu);
 
         /*
 		if(cpu->intr == 0x20) {
 			new_cpu = schedule(cpu);
 			tss[1] = (uint32_t) (new_cpu + 1);
 		} */ 
-		
+		printf("CPU struct at %p\n", (void*) cpu);		
 		if(cpu->intr >= 0x28){
 		        outb(0xa0, 0x20);
     	}
@@ -43,14 +43,14 @@ struct cpu_state* handle_irq(struct cpu_state* cpu){
 		outb(0x20, 0x20);
 		
 		printf("Interrupt: %s\n", err_msg[cpu->intr]);
-        printf("CPU status: \n eax: \t%d\n ebx: \t%d\n ecx: \t%d\n edx: \t%d\n esi: \t%d\n \
-                edi: \t%d\n ebp: \t%d\n\nInterruptnummer: \t%d\nError-Code:\t%d\n\nAutomatisch \
-                gesichert: \n eip: \t%d\n cs: \t%d\n eflags: \t%d\n esp: \t%d\n ss: \t%d\n", cpu->eax, 
+        printf("CPU status: \n eax: \t%x\n ebx: \t%x\n ecx: \t%x\n edx: \t%x\n esi: \t%x\n"
+               " edi: \t%x\n ebp: \t%x\n\nInterruptnummer: \t%x\nError-Code:\t%u\n\nAutomatisch"
+               " gesichert: \n eip: \t%x\n cs: \t%x\n eflags: \t%x\n esp: \t%x\n ss: \t%x\n", cpu->eax, 
                 cpu->ebx, cpu->ecx, cpu->edx, cpu->esi, cpu->edi, cpu->ebp, cpu->intr, cpu->error, cpu->eip, cpu->cs, cpu->eflags, cpu->esp, cpu->ss);
 		
 		asm volatile("cli; hlt");
-	}
-	
+	//}
+	printf("out of if case in irq_handler\n");
 	return cpu;
 }
 
